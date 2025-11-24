73ee548d9234ccf98a4126f8683a3e70c10f1a05
Anastasiia <a.liashchevska@gmail.com>
2025-11-24 18:31:35 +0200

    Add CustomUser model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def _create_user(self, email, password, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        if email is None:
+            raise ValueError(_('Email is required'))
+        return self._create_user(email, password, **extra_fields)
+    
+    # def create_superuser(self, email, password=None, **extra_fields):
+    #     return  self._create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
