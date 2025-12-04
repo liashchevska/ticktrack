d4db0c15c5cdc0dbbfde914858ef0566b2ae75be
Anastasiia <a.liashchevska@gmail.com>
2025-12-04 12:27:47 +0200

    Add tests for Ticket endpoints

---
+++ api/tests/test_ticket_endpoints.py
@@ -0,0 +1,74 @@
+import pytest
+from django.urls import reverse
+from api.models import CustomUser, Ticket, Board
+from model_bakery import baker
+from api.serializers import TicketSerializer
+
+
+@pytest.fixture
+def userA(db):
+    return CustomUser.objects.create_user("user_a@email.com", "password")
+
+
+@pytest.fixture
+def userB(db):
+    return CustomUser.objects.create_user("user_b@email.com", "password")
+
+
+@pytest.fixture
+def boardA(userA):
+    return baker.make(Board, owner=userA)
+
+
+@pytest.fixture
+def boardB(userB):
+    return baker.make(Board, owner=userB)
+
+
+@pytest.fixture
+def ticketsA(boardA):
+    return baker.make(Ticket, board=boardA, _quantity=2)
+
+
+@pytest.fixture
+def ticketsB(boardB):
+    return baker.make(Ticket, board=boardB, _quantity=2)
+
+
+@pytest.mark.django_db
+class TestTicketEndpoints:
+    def test_user_gets_only_his_tickets(self, client, userA, userB, ticketsA, ticketsB):
+        url = reverse("ticket-list")
+        client.force_login(userA)
+        response = client.get(url)
+
+        assert Ticket.objects.count() == len(ticketsA) + len(ticketsB)
+        assert len(response.data) == len(ticketsA)
+        assert TicketSerializer(ticketsA, many=True).data == response.data
+
+    def test_user_can_only_access_details_of_his_tickets(
+        self, client, userA, userB, ticketsA, ticketsB
+    ):
+        url = reverse("ticket-detail", kwargs={"pk": ticketsA[0].id})
+        client.force_login(userA)
+        response = client.get(url)
+        assert response.status_code == 200
+
+        client.force_login(userB)
+        response = client.get(url)
+        assert response.status_code == 404
+
+    def test_user_cant_change_tickets_board(self, client, userA, ticketsA):
+        url = reverse("ticket-detail", kwargs={"pk": ticketsA[0].id})
+        client.force_login(userA)
+        response = client.patch(url, kwargs={"board": 2})
+        assert response.data["board"] == ticketsA[0].id
+
+    def test_user_can_only_create_ticket_in_his_board(self, client, userA, boardB):
+        ticket = baker.prepare("Ticket", board=boardB)
+        ticket_data = TicketSerializer(ticket).data
+        ticket_data.pop("id")
+        url = reverse("ticket-list")
+        client.force_login(userA)
+        response = client.post(url, ticket_data)
+        assert response.status_code == 400
