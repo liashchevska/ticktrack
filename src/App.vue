94c95db013d589018e595b813c664222b783f263
Anastasiia <a.liashchevska@gmail.com>
2025-12-05 18:13:09 +0200

    Add basic login form

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
-<script setup></script>
+<script setup>
+import LoginForm from './components/LoginForm.vue';
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>In progress...</h1>
+  <LoginForm />
 </template>
 
 <style scoped></style>
