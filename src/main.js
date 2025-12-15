8cca8825f1116588782d3b561a5377bc6b6484ec
Anastasiia <a.liashchevska@gmail.com>
2025-12-15 17:10:59 +0200

    Get csrf token on startup

--- src/main.js
+++ src/main.js
@@ -3,10 +3,16 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { API } from './endpoints'
 
-const app = createApp(App)
+async function bootstrap(params) {
+  await fetch(API.CSRF, { credentials: 'include' })
 
-app.use(createPinia())
-app.use(router)
+  const app = createApp(App)
+  app.use(createPinia())
+  app.use(router)
 
-app.mount('#app')
+  app.mount('#app')
+}
+
+bootstrap()
