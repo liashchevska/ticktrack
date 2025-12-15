5b920a9079767678cefcee387593dc62b171447e
Anastasiia <a.liashchevska@gmail.com>
2025-12-15 19:27:13 +0200

    Rewrite login, signup actions

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,24 +1,37 @@
 import { ref, computed } from 'vue'
-import { API } from "@/api"
-import { request } from "@/utils/request"
-import { defineStore } from "pinia"
-import { useStorage } from '@vueuse/core'
+import { useStorage } from "@vueuse/core";
+import { defineStore } from "pinia";
+import { handleAuthResponse, request, } from '@/utils/request';
+import { API } from '@/endpoints';
+import router from '@/router';
 
 export const useAuthStore = defineStore('auth', () => {
-  const user = useStorage('user', {})
+  const user = useStorage('user', null)
   const isAuthenticated = computed(() => (Object.keys(user.value).length > 0))
+  const isVerificationPending = ref(false)
 
   async function login(payload) {
-    const temp = await request(API.AUTH.LOGIN, 'POST', payload)
-    user.value = temp.data.user
+    const response = await request(API.AUTH.LOGIN, 'POST', payload)
+    const { ok, errors, user } = handleAuthResponse(response)
+
+    if (!ok && errors.length) throw errors
+    user.value = user
   }
+
   async function signup(payload) {
-    const temp = await request(API.AUTH.SIGNUP, 'POST', payload)
+    const response = await request(API.AUTH.SIGNUP, 'POST', payload)
+    const { ok, errors, flows } = handleAuthResponse(response)
+    if (!ok && errors.length) throw errors
+    if (flows.some(flow => flow.id === 'verify_email' && flow.is_pending)) {
+      isVerificationPending.value = true
+      router.push('/email/verify')
+    }
   }
+
   async function logout() {
     await request(API.AUTH.SESSION, 'DELETE')
     user.value = null
   }
+
   return { user, isAuthenticated, login, signup, logout }
 })
-
