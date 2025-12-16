b383f2f90e90b6c36609ee28abbc9acf6803d16d
Anastasiia <a.liashchevska@gmail.com>
2025-12-16 20:07:32 +0200

    Add router guards

--- src/router/index.js
+++ src/router/index.js
@@ -3,27 +3,48 @@ import HomeView from '@/views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
 import SignupView from '@/views/SignupView.vue'
 import EmailVerificatoinView from '@/views/EmailVerificatoinView.vue'
+import { useAuthStore } from '@/stores/auth'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      component: HomeView
+      component: HomeView,
+      meta: { requiresAuth: true },
     },
     {
       path: '/login',
       component: LoginView,
+      meta: { requiresNoAuth: true }
     },
     {
       path: '/signup',
-      component: SignupView
+      component: SignupView,
+      meta: { requiresNoAuth: true }
+
     },
     {
       path: '/email/verify',
-      component: EmailVerificatoinView
+      component: EmailVerificatoinView,
+      meta: { requiresVerificationPending: true }
     }
   ],
+})
+
+
+router.beforeEach((to, from) => {
+  const { isAuthenticated, isVerificationPending } = useAuthStore()
+
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    return '/login'
+  }
+  if (to.meta.requiresVerificationPending && !isVerificationPending) {
+    return '/'
+  }
+  if (to.meta.requiresNoAuth && isAuthenticated) {
+    return '/'
+  }
 })
 
 export default router
