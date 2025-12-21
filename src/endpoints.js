2e231ae4192899e56504ca1f345bf0709562350f
Anastasiia <a.liashchevska@gmail.com>
2025-12-21 18:04:45 +0200

    Change url paths

--- src/endpoints.js
+++ src/endpoints.js
@@ -4,13 +4,13 @@ const BASE_URL = import.meta.env.VUE_APP_API_URL || 'http://127.0.0.1:8000'
 export const API = {
   CSRF: `${BASE_URL}/api/csrf/`,
   AUTH: {
-    LOGIN: `${BASE_URL}/_allauth/${CLIENT}/v1/auth/login`,
-    SESSION: `${BASE_URL}/_allauth/${CLIENT}/v1/auth/session`,
-    SIGNUP: `${BASE_URL}/_allauth/${CLIENT}/v1/auth/signup`,
-    VERIFY_EMAIL: `${BASE_URL}/_allauth/${CLIENT}/v1/auth/email/verify`,
-    VERYFY_EMAIL_RESEND: `${BASE_URL}/_allauth/${CLIENT}/v1/auth/email/verify/resend`,
-    REQUEST_PASSWORD_RESET: `${BASE_URL}/_allauth/${CLIENT}/v1/auth/password/request`,
-    RESET_PASSWORD: `${BASE_URL}/_allauth/${CLIENT}/v1/auth/password/reset`
+    LOGIN: `${BASE_URL}/auth/${CLIENT}/login/`,
+    SESSION: `${BASE_URL}/auth/${CLIENT}/session/`,
+    SIGNUP: `${BASE_URL}/auth/${CLIENT}/signup/`,
+    VERIFY_EMAIL: `${BASE_URL}/auth/${CLIENT}/email/verify/`,
+    VERYFY_EMAIL_RESEND: `${BASE_URL}/auth/${CLIENT}/email/verify/resend/`,
+    REQUEST_PASSWORD_RESET: `${BASE_URL}/auth/${CLIENT}/password/request/`,
+    RESET_PASSWORD: `${BASE_URL}/auth/${CLIENT}/password/reset/`
   },
   TICKET: {
     LIST: `${BASE_URL}/api/tickets`,
