e6194503ce2e9494640ade591343d03926b5e61b
Anastasiia <a.liashchevska@gmail.com>
2025-12-21 18:06:10 +0200

    Inplement custom signup view to handle 'accound already exists'

--- api/auth.py
+++ api/auth.py
@@ -2,20 +2,37 @@ from allauth.headless.account import views
 from django.urls.conf import path, include
 from allauth.headless.constants import Client
 from allauth.headless.account.views import SignupView
+from allauth.headless.internal.restkit.response import ErrorResponse, APIResponse
+from http import HTTPStatus
+
 
 class CustomSignupView(SignupView):
     def post(self, request, *args, **kwargs):
+        if self.input.account_already_exists:
+            return APIResponse(
+                request,
+                status=HTTPStatus.BAD_REQUEST,
+                errors=[
+                    {
+                        "param": "email",
+                        "message": "Account with specified email already exists.",
+                        "code": "account_already_exists",
+                    }
+                ],
+            )
         return super().post(request, *args, **kwargs)
 
+
 url_to_view = {
-    'login/': views.LoginView,
-    'signup/': views.SignupView,
-    'session/': views.SessionView,
-    'email/verify/': views.VerifyEmailView,
-    'email/verify/resend/': views.ResendEmailVerificationCodeView,
-    'password/request/': views.RequestPasswordResetView,
-    'password/reset': views.ResetPasswordView
+    "login/": views.LoginView,
+    "signup/": CustomSignupView,
+    "session/": views.SessionView,
+    "email/verify/": views.VerifyEmailView,
+    "email/verify/resend/": views.ResendEmailVerificationCodeView,
+    "password/request/": views.RequestPasswordResetView,
+    "password/reset": views.ResetPasswordView,
 }
+
 
 def build_urlpatterns_for_client(client):
     urlpatters = []
@@ -23,7 +40,8 @@ def build_urlpatterns_for_client(client):
         urlpatters.append(path(url, view.as_api_view(client=client)))
     return urlpatters
 
+
 urlpatterns = [
-    path('browser/', include(build_urlpatterns_for_client(Client.BROWSER))),
-    path('app/', include(build_urlpatterns_for_client(Client.APP))),
+    path("browser/", include(build_urlpatterns_for_client(Client.BROWSER))),
+    path("app/", include(build_urlpatterns_for_client(Client.APP))),
 ]
