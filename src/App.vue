6ea4bc508c2aa04c36f98a4064ba469d2bef366e
Anastasiia <a.liashchevska@gmail.com>
2026-01-01 20:28:27 +0200

    Move LogoutButton to App

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
-<script setup>
-
-</script>
-
 <template>
+  <header>
+    <LogoutButton v-if="isAuthenticated" />
+  </header>
   <main>
     <RouterView />
   </main>
-  <!-- <LoginForm /> -->
 </template>
+
+<script setup>
+import { storeToRefs } from 'pinia'
+import LogoutButton from './components/LogoutButton.vue'
+import { useAuthStore } from './stores/auth'
+
+const { isAuthenticated } = storeToRefs(useAuthStore())
+</script>
 
 <style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,9 @@
 <template>
-  <div>Home Page</div>
-  <LogoutButton v-if="auth.isAuthenticated" />
-
-  <BaseList :item-list="boards.boardList" @itemClicked="openBoardView" />
-  <BoardForm />
   <RouterView />
 </template>
 
 <script setup>
+import { RouterView } from 'vue-router'
 
-import BaseList from '@/components/BaseList.vue';
-import BoardForm from '@/components/Board/BoardForm.vue';
-import LogoutButton from '@/components/LogoutButton.vue';
-import router from '@/router';
-import { useAuthStore } from '@/stores/auth';
-import { useBoardsStore } from '@/stores/board';
-import { RouterView } from 'vue-router';
 
-const auth = useAuthStore()
-const boards = useBoardsStore()
-
-function openBoardView(id) {
-  router.push(`/${id}`)
-}
 </script>
