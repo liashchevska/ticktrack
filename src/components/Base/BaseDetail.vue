3c7ac051aa1fa28d28a2a3b7b99d0ccc33abc5e3
Anastasiia <a.liashchevska@gmail.com>
2026-01-12 21:05:54 +0200

    Add BaseDetail component

---
+++ src/components/Base/BaseDetail.vue
@@ -0,0 +1,12 @@
+<template>
+  <!-- <slot name="header"></slot> -->
+  <slot></slot>
+  <slot name="actions">
+    <button @click="emit('delete')">Delete</button>
+  </slot>
+
+</template>
+
+<script setup>
+const emit = defineEmits(['delete'])
+</script>
