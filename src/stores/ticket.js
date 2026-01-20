1bc3283e383a69b5d1cd709a6751817433819f87
Anastasiia <a.liashchevska@gmail.com>
2026-01-20 17:20:59 +0200

    Set up ticketStore

---
+++ src/stores/ticket.js
@@ -0,0 +1,55 @@
+import { API } from '@/endpoints';
+import { request } from '@/utils/request';
+import { defineStore } from 'pinia';
+import { ref } from 'vue'
+
+export const useTicketStore = defineStore('ticket', () => {
+  const ticketsByBoard = ref({})
+
+  function getTickets(boardId) {
+    const boardTickets = ticketsByBoard.value[boardId]
+    if (!boardTickets) return []
+    return Object.values(boardTickets)
+  }
+
+  async function fetchTickets(boardId) {
+    const { ok, data, errors } = await request(API.TICKET.LIST({ 'board_id': boardId }))
+    if (!ok) throw { errors }
+    ticketsByBoard.value[boardId] = data.reduce((accumulator, item) => {
+      accumulator[item.id] = item
+      return accumulator
+    }, {})
+  }
+
+  async function createTicket(boardId, payload) {
+    const { ok, errors, data } = await request(API.TICKET.LIST, 'POST', payload)
+    if (!ok) throw { errors }
+    ticketsByBoard.value[boardId] ??= {}
+    ticketsByBoard.value[boardId][data.id] = data
+  }
+
+  async function updateTicket(ticketId, payload) {
+    const { ok, errors, data } = await request(API.TICKET.DETAIL(ticketId), 'POST', payload)
+    if (!ok) throw { errors }
+    const boardId = data.board_id
+    const board = ticketsByBoard.value[boardId]
+    if (!board) return
+    ticketsByBoard.value[boardId][ticketId] = data
+  }
+
+  async function deleteTicket(boardId, ticketId) {
+    const { ok, errors } = await request(API.TICKET.DETAIL(ticketId), 'DELETE')
+    if (!ok) throw { errors }
+    const board = ticketsByBoard.value[boardId]
+    delete board[ticketId]
+  }
+
+  return {
+    ticketsByBoard,
+    getTickets,
+    fetchTickets,
+    createTicket,
+    updateTicket,
+    deleteTicket
+  }
+})
