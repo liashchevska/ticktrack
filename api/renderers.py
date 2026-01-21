8268895666903cd91de05e6e8932dce8c6ce5e27
Anastasiia <a.liashchevska@gmail.com>
2026-01-21 21:21:45 +0200

    Avoid double-wrapping if key already exists

--- api/renderers.py
+++ api/renderers.py
@@ -2,11 +2,13 @@ from rest_framework.renderers import JSONRenderer
 
 class ApiRenderer(JSONRenderer):
     def render(self, data, accepted_media_type=None, renderer_context=None):
+        if data is None:
+            return super().render(data, accepted_media_type, renderer_context)
         status_code = renderer_context['response'].status_code
         is_ok = 200 <= status_code < 300
         payload_key = "data" if is_ok else "errors"
-        response = {
-            "status": status_code,
-            payload_key: data    
-        }
+        if isinstance(data, dict) and payload_key in data:
+            response = {"status": status_code, **data}
+        else:
+            response = {"status": status_code, payload_key: data}
         return super(ApiRenderer, self).render(response, accepted_media_type, renderer_context)
