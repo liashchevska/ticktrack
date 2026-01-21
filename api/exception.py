f4fee2434ed082f28272e9ae2c0966a5e12cf850
Anastasiia <a.liashchevska@gmail.com>
2026-01-21 20:32:28 +0200

    Only format errors for validation exceptions

--- api/exception.py
+++ api/exception.py
@@ -1,18 +1,22 @@
 from rest_framework.views import exception_handler
+from rest_framework.exceptions import ValidationError
+
 
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
-    response.data = format_errors(exc.detail)
+    if (
+        isinstance(exc, ValidationError)
+        and response is not None
+        and isinstance(response.data, dict)
+    ):
+        response.data = format_errors(response.data)
     return response
+
 
 # Handle non-field errors
 def format_errors(errors):
     formated = []
     for field, error_list in errors.items():
         for error in error_list:
-            formated.append({
-                'param': field,
-                'message': str(error),
-                'code': error.code
-            })
+            formated.append({"param": field, "message": str(error), "code": error.code})
     return formated
