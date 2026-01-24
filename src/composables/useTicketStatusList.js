fbe40d9b9047b9dd2ee076ec908f37d72a8e6d0e
Anastasiia <a.liashchevska@gmail.com>
2026-01-24 19:32:07 +0200

    Expose isInitialized

--- src/composables/useTicketStatusList.js
+++ src/composables/useTicketStatusList.js
@@ -3,19 +3,19 @@ import { request } from '@/utils/request'
 import { ref } from 'vue'
 
 const ticketStatusList = ref({})
-let isInitialized = false
+const isInitialized = ref(false)
 
 export function useTicketStatusList() {
   async function fetchTicketStatusList() {
-    if (!isInitialized) {
+    if (!isInitialized.value) {
       const { ok, errors, data } = await request(API.META.TICKET_STATUS_LIST, 'GET')
       if (!ok) { throw errors }
       ticketStatusList.value = data.reduce((accumulator, item) => {
         accumulator[item.status] = item.label
         return accumulator
       }, {})
-      isInitialized = true
+      isInitialized.value = true
     }
   }
-  return { ticketStatusList, fetchTicketStatusList }
+  return { isInitialized, ticketStatusList, fetchTicketStatusList }
 }
