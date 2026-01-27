9468ad4702de04622f00fe0e159018c2d734e09d
Anastasiia <a.liashchevska@gmail.com>
2026-01-27 21:38:51 +0200

    Implement ConfirmDialog for destructive actions

---
+++ src/components/Base/ConfirmDialog.vue
@@ -0,0 +1,30 @@
+<template>
+  <BaseModal v-model="isOpen" :title="props.title">
+    <p>{{ props.message }}</p>
+    <template #actions>
+      <button @click="confirm">Confirm</button>
+      <button @click="isOpen = false">Cancel</button>
+    </template>
+  </BaseModal>
+
+</template>
+
+<script setup>
+import BaseModal from './BaseModal.vue';
+const isOpen = defineModel()
+const props = defineProps({
+  title: {
+    type: String,
+    default: 'Confirm action'
+  },
+  message: {
+    type: String,
+    default: 'Are you sure?'
+  }
+})
+const emit = defineEmits(['confirm'])
+function confirm() {
+  emit('confirm')
+  isOpen.value = false
+}
+</script>
