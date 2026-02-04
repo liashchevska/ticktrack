c2b8786d0cfd2208f68e12aa99a04a28dc426d29
Anastasiia <a.liashchevska@gmail.com>
2026-02-04 19:27:05 +0200

    Add current user's email to header, apply base css

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
-  <header>
-    <LogoutButton v-if="isAuthenticated" />
+  <header class="app__header">
+    <template v-if="isAuthenticated">
+      <span class="app__user">{{ user.email }}</span>
+      <LogoutButton class="app__logout" />
+    </template>
   </header>
   <main>
     <RouterView />
   </main>
+  <footer></footer>
 </template>
 
 <script setup>
@@ -12,7 +16,23 @@ import { storeToRefs } from 'pinia'
 import LogoutButton from './components/LogoutButton.vue'
 import { useAuthStore } from './stores/auth'
 
-const { isAuthenticated } = storeToRefs(useAuthStore())
+const { isAuthenticated, user } = storeToRefs(useAuthStore())
 </script>
 
-<style scoped></style>
+<style lang="css">
+#app {
+  display: flex;
+  flex-direction: column;
+}
+
+.app__header {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 1rem;
+}
+
+.app__user {
+  margin-right: 1rem;
+}
+</style>
