4afed1d47b46883fa8ec1be1a661c61a66711eaf
Anastasiia <a.liashchevska@gmail.com>
2026-02-12 14:44:01 +0200

    Update TicketInList structure

--- src/components/Ticket/TicketInList.vue
+++ src/components/Ticket/TicketInList.vue
@@ -1,9 +1,16 @@
 <template>
-  <h2>{{ ticket.title }} {{ ticket.status }}</h2>
-
+<div class="ticket">
+    <div class="ticket__header">
+  <h2>{{ ticket.title }}</h2>
+<div class="actions">
   <button @click="isUpdateOpen = true">Edit ticket</button>
   <button @click="isConfirmOpen = true">Delete ticket</button>
-
+</div>
+    </div>
+    <div class="ticket__description">
+      {{ ticket.description }}
+    </div>
+  </div>
   <ConfirmDialog v-model="isConfirmOpen" @confirm="onTicketDelete(ticket.id)" />
   <TicketFormModal v-model="isUpdateOpen" :ticket="props.ticket" @updated="isUpdateOpen = false" />
 </template>
