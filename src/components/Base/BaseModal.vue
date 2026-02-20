4adf4ce433d2966de9d58ad990a0d9713d7292bd
Anastasiia <a.liashchevska@gmail.com>
2026-02-20 21:00:36 +0200

    Add initial styles for BaseModal

--- src/components/Base/BaseModal.vue
+++ src/components/Base/BaseModal.vue
@@ -17,7 +17,7 @@
   </Transition>
 </template>
 <script setup>
-import { onMounted, onUnmounted } from 'vue'
+import { onMounted, onUnmounted, watch } from 'vue'
 import CloseButton from './CloseButton.vue'
 
 defineProps({
@@ -29,8 +29,72 @@ function closeOnEscape(event) {
   if (event.key !== 'Escape') return
   isOpen.value && (isOpen.value = false)
 }
-onMounted(() => window.addEventListener('keyup', closeOnEscape))
-onUnmounted(() => window.removeEventListener('keyup', closeOnEscape))
+
+watch(isOpen, (open) => {
+  document.body.style.overflow = open ? 'hidden' : ''
+})
+
+onMounted(() => {
+  window.addEventListener('keyup', closeOnEscape)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('keyup', closeOnEscape)
+  document.body.style.overflow = ''
+})
+
 </script>
 
-<style lang="css"></style>
+<style lang="css">
+.modal__backdrop {
+  position: fixed;
+  inset: 0;
+  z-index: var(--z-modal);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--modal-backdrop);
+  backdrop-filter: blur(3px);
+}
+
+.modal {
+  display: flex;
+  flex-direction: column;
+  max-width: 30rem;
+  max-height: 90vh;
+  width: 100%;
+  overflow: hidden;
+  border-radius: var(--radius-sm);
+
+  background-color: white;
+}
+
+
+.modal__header,
+.modal__footer {
+  display: flex;
+  align-items: center;
+  flex-shrink: 0;
+}
+
+.modal__header {
+  justify-content: space-between;
+  background: var(--modal-header);
+}
+
+.modal__footer {
+  justify-content: flex-end;
+}
+
+.modal__body {
+  overflow-y: auto;
+  flex: 1;
+  min-height: 0;
+}
+
+.modal__header,
+.modal__body,
+.modal__footer {
+  padding: var(--space-lg) var(--space-xl);
+}
+</style>
