faa5b7f1a4a0435bf179898b3929977431f7c7d8
Anastasiia <a.liashchevska@gmail.com>
2026-03-03 19:50:40 +0200

    Move create button from footer to header in BoardList

--- src/views/BoardListView.vue
+++ src/views/BoardListView.vue
@@ -1,27 +1,28 @@
 <template>
   <nav class="board-list" aria-label="Board list navigation">
     <div class="board-list__header">
-      <h1 class="board-list__title">Your boards</h1>
-      <div v-if="!isDesktop" class="board-list__actions">
-        <IconButton @click="isOpen = !isOpen">
-          <ChevronDownIcon class="icon-button__icon icon-button__chevron "
-            :class="{ 'icon-button__chevron--open': isOpen }" />
-        </IconButton>
-        <IconButton @click="isCreateOpen = true">
-          <PlusIcon class="icon-button__icon" />
-        </IconButton>
+      <h1 class="board-list__title">Boards</h1>
+      <div class="board-list__actions">
+        <template v-if="isDesktop">
+          <button class="btn board-list__add" @click="isCreateOpen = true">+ New</button>
+        </template>
+        <template v-else>
+          <IconButton @click="isOpen = !isOpen">
+            <ChevronDownIcon class="icon-button__icon icon-button__chevron "
+              :class="{ 'icon-button__chevron--open': isOpen }" />
+          </IconButton>
+          <IconButton @click="isCreateOpen = true">
+            <PlusIcon class="icon-button__icon" />
+          </IconButton>
+        </template>
       </div>
     </div>
     <BaseList class="board-list__main" :item-list="boardList" :active-id="activeBoard?.id" @itemClicked="openBoardView"
       v-show="isOpen">
       <template #default="{ item, isActive }">
         {{ item.title }}
-        <!-- <small v-if="isActive">(active)</small> -->
       </template>
     </BaseList>
-    <div v-if="isDesktop" class="board-list__footer">
-      <button class="btn" @click="isCreateOpen = true">Create board</button>
-    </div>
     <BoardFormModal v-model="isCreateOpen" @created="isCreateOpen = false" title="Create" />
   </nav>
 </template>
@@ -77,11 +78,22 @@ function openBoardView(id) {
   display: flex;
   justify-content: space-between;
   align-items: center;
+  padding: var(--space-md) 0;
 }
 
 .board-list__actions {
   display: flex;
   gap: var(--space-xs);
+}
+
+.board-list__add {
+  background: none;
+  font-size: var(--text-xl);
+}
+
+.btn.board-list__add:hover {
+  box-shadow: none;
+  background: rgba(0, 0, 0, 0.05);
 }
 
 .board-list .list {
@@ -92,6 +104,11 @@ function openBoardView(id) {
 .board-list .list__item--active {
   list-style-type: disc;
   font-weight: 700;
+}
+
+.board-list__main {
+  padding-top: var(--space-sm);
+  border-top: 1px solid var(--color-border-dark);
 }
 
 @media (min-width: 1024px) {
@@ -105,31 +122,5 @@ function openBoardView(id) {
   .board-list__main {
     overflow-y: auto;
   }
-
-  .board-list__footer {
-    margin-top: auto;
-    display: flex;
-    justify-content: center;
-    padding: var(--space-lg) 0;
-  }
-
-  .board-list__footer button {
-    width: 100%;
-  }
-
-  .board-list__title {
-    position: relative;
-  }
-
-  .board-list__title::after {
-    content: "";
-    position: absolute;
-    left: 0;
-    bottom: calc(-1 * var(--space-sm));
-    width: 100%;
-    height: 1px;
-    background-color: var(--seperator);
-  }
-
 }
 </style>
