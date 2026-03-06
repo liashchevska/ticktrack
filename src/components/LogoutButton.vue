16850df828be29b6123159f2441e28487abda500
Anastasiia <a.liashchevska@gmail.com>
2026-03-06 20:59:06 +0200

    Add confirmation dialog before logout

--- src/components/LogoutButton.vue
+++ src/components/LogoutButton.vue
@@ -1,13 +1,17 @@
 <template>
-  <button class="btn btn--primary" @click="logout">Logout</button>
+  <button class="btn btn--primary" @click="isConfirmOpen = true">Logout</button>
+  <ConfirmDialog v-model="isConfirmOpen" @confirm="logout" />
 </template>
 
 <script setup>
 import { useRouter } from 'vue-router'
 import { useAuthStore } from '@/stores/auth';
+import ConfirmDialog from './Base/ConfirmDialog.vue';
+import { ref } from 'vue';
 
 const router = useRouter()
 const auth = useAuthStore()
+const isConfirmOpen = ref(false)
 
 async function logout() {
   await auth.logout()
