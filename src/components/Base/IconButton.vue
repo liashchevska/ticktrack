e0073b242603a7a779472e86e03bf00a6476e687
Anastasiia <a.liashchevska@gmail.com>
2026-03-08 21:47:05 +0200

    Set width to .icon-button

--- src/components/Base/IconButton.vue
+++ src/components/Base/IconButton.vue
@@ -14,12 +14,14 @@
   align-items: center;
   justify-content: center;
 
+  width: var(--icon-size);
+  height: var(--icon-size);
   padding: 0;
 }
 
 .icon-button__icon {
-  width: var(--icon-size);
-  height: var(--icon-size);
+  width: 100%;
+  height: 100%;
   display: block;
 }
 </style>
