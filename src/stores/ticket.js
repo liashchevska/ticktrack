0452467b6277b89a628993831e8475b26084677b
Anastasiia <a.liashchevska@gmail.com>
2026-03-14 19:40:08 +0200

    Use map instead of object to preserve tickets order

--- src/stores/ticket.js
+++ src/stores/ticket.js
@@ -9,24 +9,25 @@ export const useTicketStore = defineStore('ticket', () => {
   function getTickets(boardId) {
     const boardTickets = ticketsByBoard.value[boardId]
     if (!boardTickets) return []
-    return Object.values(boardTickets)
+    return Array.from(boardTickets.values())
   }
 
   async function fetchTickets(boardId) {
     const { ok, data, errors } = await request(API.TICKET.FILTER({ 'board_id': boardId }))
     if (!ok) { throw errors }
     ticketsByBoard.value[boardId] = data.reduce((accumulator, item) => {
-      accumulator[item.id] = item
+      accumulator.set(item.id, item)
       return accumulator
-    }, {})
+    }, new Map())
   }
 
   async function createTicket(boardId, payload) {
     payload.board = boardId
     const { ok, errors, data } = await request(API.TICKET.LIST, 'POST', payload)
     if (!ok) { throw errors }
-    ticketsByBoard.value[boardId] ??= {}
-    ticketsByBoard.value[boardId][data.id] = data
+
+    ticketsByBoard.value[boardId] ??= new Map()
+    ticketsByBoard.value[boardId].set(data.id, data)
   }
 
   async function updateTicket(ticketId, payload) {
@@ -35,14 +36,14 @@ export const useTicketStore = defineStore('ticket', () => {
     const boardId = data.board
     const board = ticketsByBoard.value[boardId]
     if (!board) return
-    ticketsByBoard.value[boardId][ticketId] = data
+    ticketsByBoard.value[boardId].delete(data.id)
+    ticketsByBoard.value[boardId].set(data.id, data)
   }
 
   async function deleteTicket(boardId, ticketId) {
     const { ok, errors } = await request(API.TICKET.DETAIL(ticketId), 'DELETE')
     if (!ok) { throw errors }
-    const board = ticketsByBoard.value[boardId]
-    delete board[ticketId]
+    ticketsByBoard.value[boardId].delete(ticketId)
   }
 
   return {
