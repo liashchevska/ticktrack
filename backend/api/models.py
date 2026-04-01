2b2b941a21515d45f88ecb1488385e25d17c737a
Anastasiia <a.liashchevska@gmail.com>
2026-04-01 20:25:12 +0300

    Add method to create unique demo user

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
+import uuid
+from allauth.account.models import EmailAddress
 
 class CustomUserManager(BaseUserManager):
     def _create_user(self, email, password, **extra_fields):
@@ -16,6 +18,12 @@ class CustomUserManager(BaseUserManager):
         if email is None:
             raise ValueError(_('Email is required'))
         return self._create_user(email, password, **extra_fields)
+
+    def create_demo_user(self): 
+        userid = uuid.uuid4()    
+        user = self.create_user(f"{userid}@demo.com", is_demo=True)
+        EmailAddress.objects.create(user=user, email=user.email, verified=True, primary=True)
+        return user
 
 
 class CustomUser(AbstractUser):
